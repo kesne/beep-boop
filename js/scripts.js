df5e71ea61efff52ff4e5a4e9e9146fb3083418e
Jaime Gensler <jaimegensler0@gmail.com>
2019-11-01 09:19:30 -0700

    create function that returns array of numbers from 0 to user's input

--- js/scripts.js
+++ js/scripts.js
@@ -1,11 +1,20 @@
 //Business Logic
+function skynet(number) {
+  var robotArray = [];
+  for (var i = 0; i <= number; i++) {
+    robotArray.push(i);
+  }
+  return robotArray;
+}
+
 
 //UI Logic
 $(document).ready(function () {
   $("form#user-input").submit(function(event) {
     event.preventDefault();
-    
+
     var userNumber = $("input#number").val();
-    alert("Your input was: " + userNumber);
-  })
-})
+    var result = skynet(userNumber)
+    console.log(result);
+  });
+});
