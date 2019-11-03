3482ca8999d9f5de97789ab5430493e362168c89
Jaime Gensler <jaimegensler0@gmail.com>
2019-11-03 09:52:24 -0800

    remove unnecessary variables, slightly clean up front-end code

--- js/scripts.js
+++ js/scripts.js
@@ -1,15 +1,9 @@
 //Business Logic
-var greeting = "Greetings, human. I am designated 'RoboBot' - you may call me 'Rob.' What is your designator?";
-var badName = "' sounds more like a robot name. Perhaps it is you that is the robot, and I, the human?";
-
 function numberGame(number, name) {
-  if (isNaN(number) || number < 0 || (Math.floor(number) != number) || number > 120) {
-    return "ERROR! Please enter a positive integer from 1 to 120."
-  }
   var robotArray = [];
   for (var i = 0; i <= number; i++) {
-    // Modified guidelines to make it a 2/3 chance the number will be changed
-    if (Math.random() > 0.67) {
+    // Modified guidelines to make it a ~80% chance the number will be changed
+    if (Math.random() > 0.8) {
       robotArray.push(i);
     } else if ((i + "").includes("3")) {
       robotArray.push("I'm sorry, " + name + ". I'm afraid I can't do that.");
@@ -38,19 +32,20 @@ function roboTalk(sentence, location, speed, i) {
 $(document).ready(function () {
   var userName = "";
   var userNumber = "";
-  roboTalk(greeting, $("p#chat"), 50, 0);
+  roboTalk("Greetings, human. I am designated 'RoboBot' - you may call me 'Rob.' What is your designator?", $("p#chat"), 50, 0);
 
   $("form#prompt-name").submit(function(event) {
     event.preventDefault();
 
     userName = $("input#name").val();
     $("p#chat").text("");
+
     if (/[^a-z ]/i.test(userName)) {
-      roboTalk("'" + userName + badName, $("p#chat"), 50, 0);
+      roboTalk("'" + userName + "' sounds more like a robot name. Perhaps it is you that is the robot, and I, the human?", $("p#chat"), 50, 0);
     } else {
-    $("form#prompt-name").hide();
-    $("form#prompt-number").show();
-    roboTalk("It is a pleasure to make your acquaintance, '" + userName + ".' Let us play a game. What is your favorite number?", $("p#chat"), 50, 0);
+      $("form#prompt-name").hide();
+      $("form#prompt-number").show();
+      roboTalk("It is a pleasure to make your acquaintance, '" + userName + ".' Let us play a game. What is your favorite number?", $("p#chat"), 50, 0);
     }
   });
 
@@ -58,11 +53,14 @@ $(document).ready(function () {
     event.preventDefault();
 
     userNumber = parseInt($("input#number").val());
-    var result = numberGame(userNumber, userName);
-
     $("p#chat").text("");
     $("p#output").text("");
-    roboTalk(result, $("p#output"), 2, 0);
-  });
 
+    if ((isNaN(userNumber)) || (userNumber < 0) || (Math.floor(userNumber) != userNumber) || (userNumber > 120)) {
+      roboTalk("ERROR! Please enter a positive integer from 1 to 120.", $("p#chat"), 30, 0);
+    } else {
+      var result = numberGame(userNumber, userName);
+      roboTalk(result, $("p#output"), 2, 0);
+    }
+  });
 });
